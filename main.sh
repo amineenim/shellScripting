72e7f7f01ef78609858dd312d27d467b455ff37b
jeankh <66411823+jeankh@users.noreply.github.com>
2022-12-11 17:26:29 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,7 +10,7 @@ case $1 in
         --help) 
         echo -e  "
                 add_image : allows you to add an image either by directly defining the imageName and exstension \n\t\t\t or defining an absolute path to your image\n
-                build/path/to/dir : allows you to build your project in a specific firectory either by defining \n\t\t\t an absolute path or relative path to working directory\n
+                build/(path/to/dir) : allows you to build your project in a specific firectory either by defining \n\t\t\t an absolute path or relative path to working directory\n
                 --auth : allows you to either authenticate to the app or register\n
                 --help : displays a listing of all available options for the script and their structres\n"
                 
